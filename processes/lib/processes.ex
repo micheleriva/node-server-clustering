667be5c1241b2b9f889cd0da9e954452d8d43923
Michele Riva <master@micheleriva.it>
2018-11-10 16:21:22 +0100

    wip

--- processes/lib/processes.ex
+++ processes/lib/processes.ex
@@ -4,7 +4,7 @@ defmodule Processes do
   def sendRequest url do
     case HTTPoison.post url, [], [], [timeout: 50_000, recv_timeout: 50_000] do
       {:ok,    %HTTPoison.Response{status_code: 200, headers: headers}} 
-          -> IO.inspect {headers}
+          -> IO.inspect headers
       {:error, %HTTPoison.Error{reason: reason}}                  
           -> IO.inspect {:error, reason}
     end
